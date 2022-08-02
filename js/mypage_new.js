79883fffe8d875e7f4bfa2550f04c72f361b1d31
seon5min <seon5.min@gmail.com>
2022-08-02 18:09:00 +0900

    aaa

---
+++ js/mypage_new.js
@@ -0,0 +1,23 @@
+$(function () {
+    // $('.topten .ico_arrow').on('click', function () {
+    //   $('.topten').addClass('list_open');
+    //   $('.radio').removeClass('radio_open');
+    //   $('.recommend_wrap .top_container').removeClass('label_triangle');
+    //   if ($('.m_label_arrow').css({ 'transform': 'rotate(180deg)'}, 5000)) {
+    //     $('.m_label_arrow').css({ 'transform': 'rotate(0deg)'}, 5000)
+    //   }
+    //   rankSwiper.slideTo(1, 0, false);
+    //   rankSwiper.autoplay.stop();
+    //   return false;
+    // });
+    $('.box_con.status_con .status_info').on('click', function () { $('.box_con.status_con .status_layer').css('display', 'flex'); });
+    $('.box_con.status_con .status_close').on('click', function () { $('.box_con.status_con .status_layer').css('display', 'none'); });
+
+    $('.box_con.reading_activities_con .reading_info').on('click', function () { $('.box_con.reading_activities_con .info_layer').css('display', 'flex'); });
+    $('.box_con.reading_activities_con .info_close').on('click', function () { $('.box_con.reading_activities_con .info_layer').css('display', 'none'); });
+
+    $('.box_con.friend_status_con .status_info').on('click', function () { $('.box_con.friend_status_con .friend_layer').css('display', 'flex'); });
+    $('.box_con.friend_status_con .friend_close').on('click', function () { $('.box_con.friend_status_con .friend_layer').css('display', 'none'); });
+
+
+});
--- js/ui_chart.js
+++ js/ui_chart.js
@@ -1,11 +1,120 @@
-const config = {
-	type: 'bar',
-	data: data,
-	options: {
-	  scales: {
-		y: {
-		  beginAtZero: true
-		}
-	  }
-	},
-  };
+			/* 차트 */
+			function chartDataStatus(chartLabel, chartData) {
+				var chartDataset = null;
+				var chartOption = null;
+				chartDataset = {
+					labels: chartLabel,
+					datasets: [{
+						type: 'bar',
+						barThickness: 40,
+						data: chartData,
+						backgroundColor: [
+							'#583BF8',
+							'#F3D144',
+							'#F57E25',
+							'#A363F8'
+						],
+						borderRadius: 8,
+					}]
+				};
+
+				// Options
+				chartOption = {
+					responsive: false,
+					legend: {
+						display: false
+					},
+					plugins: {
+						// tooltip
+						tooltip: {
+							displayColors: true,
+							titleFont: {
+								size: 14,
+								weight: 'lighter',
+								lineHeight : 1
+							  },
+							  bodyFont: {
+								size: 14,
+								weight: 'lighter',
+								lineHeight : '14px'
+							  },
+							borderColor: '#DEDEDE',
+							 backgroundColor:'#fff',
+							 borderWidth: 1,
+							 titleColor:'#000',
+							 titleAlign:'center',
+							 boxWidth:0,
+							 boxHeight:0,
+							 bodyAlign: 'center',
+							 usePointStyle: true, 
+							 yAlign: "bottom",	
+							 padding: 15,	
+							 callbacks: {
+								labelTextColor: function(context){
+									return myChart.data.datasets.backgroundColor;
+								}
+							 },							 					 
+						},
+						legend: false,
+						title: {
+							display: true,
+							text: '초급 Lv 03 일별 평균 학습현황',
+							font: {
+								size: 14,
+								family: "'Noto Sans KR','맑은 고딕','Malgun Gothic','Roboto'",
+								weight: '400',
+								color: '#717171'
+
+							},
+
+							align: 'end'
+						}
+					},
+					layout: {
+						padding: {
+		
+							left: 0,
+							right: 0,
+							top: 0,
+							bottom: 0
+						}
+					},
+					scales: {
+						x: {
+
+
+						},
+						y: {
+							// y축 옵션
+							suggestedMin: 0,
+							suggestedMax: 1,
+							ticks: {
+								stepSize: 10,
+								beginAtZero: true,
+								color: "#717171",
+								font: {
+									size: 12,
+									family: "'Noto Sans KR','맑은 고딕','Malgun Gothic','Roboto'"
+								},
+								padding: 5,
+
+
+							},
+							grid: {
+								color: "#e1e1e1",
+								display: true,
+								// drawBorder: false,
+								// drawTicks: false,
+								// zeroLineColor: "transparent",
+
+							},
+						},
+					},
+				}
+				var chartSet = {
+					type: 'bar',
+					data: chartDataset,
+					options: chartOption,
+				}
+				return chartSet;
+			}
